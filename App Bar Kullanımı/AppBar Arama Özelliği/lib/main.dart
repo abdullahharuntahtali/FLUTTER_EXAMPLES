41b95d8a1d7fa0a5e7695e409e65a05ef7bf7abd
Abdullah Harun TAHTALI <abdullahharuntahtali0@gmail.com>
2021-12-31 11:35:40 +0300

    Add files via upload

---
+++ App Bar Kullanımı/AppBar Arama Özelliği/lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  bool aramaYapiliyorMu=false;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: aramaYapiliyorMu ?
+            TextField(
+              decoration: InputDecoration(hintText: "Arama için birşey yazın"),
+              onChanged: (aramaSonucu){
+                print("Arama Sonucu : $aramaSonucu");
+              },
+            )
+            :Text("AppBar Arama"),
+        actions: [
+          aramaYapiliyorMu ?
+              IconButton(
+              icon: Icon(Icons.cancel),
+              onPressed: (){
+                setState(() {
+                  aramaYapiliyorMu=false;
+                });
+              },
+            )
+           : IconButton(
+            icon: Icon(Icons.search),
+            onPressed: (){
+              setState(() {
+                aramaYapiliyorMu=true;
+              });
+            },
+          ),
+        ],
+
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+          ],
+        ),
+      ),
+
+    );
+  }
+}
